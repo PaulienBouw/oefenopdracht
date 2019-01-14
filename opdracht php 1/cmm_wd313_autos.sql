06bd93b20c469a34286e1754381a6a9d1247e245
PAULIEN-LAPTOP-\Paulien <paulien.bouw@outlook.com>
2019-01-14 13:16:48 +0100

    nieuwe opdracht php

---
+++ opdracht php 1/cmm_wd313_autos.sql
@@ -0,0 +1,165 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Gegenereerd op: 10 jul 2018 om 21:35
+-- Serverversie: 5.6.34-log
+-- PHP-versie: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cmm_wd313_autos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `autos`
+--
+
+CREATE TABLE `autos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `merk_id` int(11) UNSIGNED DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `kleur` varchar(100) DEFAULT NULL,
+  `brandstof` varchar(100) DEFAULT NULL,
+  `zitplaatsen` int(2) DEFAULT NULL,
+  `prijs` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `autos`
+--
+
+INSERT INTO `autos` (`id`, `merk_id`, `title`, `type`, `kleur`, `brandstof`, `zitplaatsen`, `prijs`) VALUES
+(1, 6, 'Eerste versie', 'Model 3', 'zwart', 'electra', 5, 590000),
+(2, 2, 'Lekker offroad', 'XC70', 'grijs', 'diesel', 5, 49000),
+(3, 2, 'Gezin', 'V70', 'wit', 'Hybrid', 5, 59000),
+(4, 3, 'Luxe', '5', 'blauw', 'Hybrid', 5, 79000),
+(5, 5, 'First Sight', 'C8 Preliator', 'blauw', 'Hybrid', 2, 129000),
+(6, 3, 'Instap model', '1', 'roze', 'Benzine', 4, 15000),
+(7, 1, 'Het begin', '600', 'Geel', 'benzine', 4, 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `landen`
+--
+
+CREATE TABLE `landen` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `land_id` int(11) UNSIGNED DEFAULT NULL,
+  `land` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `landen`
+--
+
+INSERT INTO `landen` (`id`, `land_id`, `land`) VALUES
+(1, 1, 'Nederland'),
+(2, 2, 'Zweden'),
+(3, 3, 'Duitsland'),
+(4, 4, 'Nederland'),
+(5, 5, 'Nederland'),
+(6, 6, 'Amerika');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `merken`
+--
+
+CREATE TABLE `merken` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `naam` varchar(255) DEFAULT NULL,
+  `land` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `merken`
+--
+
+INSERT INTO `merken` (`id`, `naam`, `land`) VALUES
+(1, 'Daf', 'Nederland'),
+(2, 'Volvo', 'Zweden'),
+(3, 'BMW', 'Duitsland'),
+(4, 'Burton', 'Nederland'),
+(5, 'Spyker', 'Nederland'),
+(6, 'Tesla', 'Amerika');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `autos`
+--
+ALTER TABLE `autos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `merk_id` (`merk_id`);
+
+--
+-- Indexen voor tabel `landen`
+--
+ALTER TABLE `landen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `merk_id` (`land_id`);
+
+--
+-- Indexen voor tabel `merken`
+--
+ALTER TABLE `merken`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `autos`
+--
+ALTER TABLE `autos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT voor een tabel `landen`
+--
+ALTER TABLE `landen`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT voor een tabel `merken`
+--
+ALTER TABLE `merken`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `autos`
+--
+ALTER TABLE `autos`
+  ADD CONSTRAINT `autos_ibfk_1` FOREIGN KEY (`merk_id`) REFERENCES `merken` (`id`);
+
+--
+-- Beperkingen voor tabel `landen`
+--
+ALTER TABLE `landen`
+  ADD CONSTRAINT `landen_ibfk_1` FOREIGN KEY (`land_id`) REFERENCES `merken` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ opdracht php 1/form.php
@@ -0,0 +1,126 @@
+﻿<html>
+<head>
+<title>Huiswerk pagina opdracht 12</title>
+<!--<link href="css/css-reset.css" rel="stylesheet" type="text/css">
+<link href="css/styles.css" rel="stylesheet" type="text/css"> -->
+</head>
+<body>
+<header>
+	<h1>Welkom bij mijn oefenopdracht</h1>
+</header>
+<main>
+<?php
+ini_set('error.reporting', E_ALL);
+ini_set('display_errors', false);
+
+$br = "<br>\n";
+	
+$localhost  = "localhost"; 
+$username   = "root"; 
+$password   = "root"; 
+$dbname     = "cmm_autos";  
+$con = new mysqli($localhost, $username, $password, $dbname); 
+if( $con->connect_error){
+   die('Error: ' . $con->connect_error);
+} 
+$sql = 'SELECT merken.id, merken.naam, merken.land, autos.title, autos.kleur, autos.type, autos.brandstof 
+		FROM merken 
+		LEFT JOIN autos ON merk_id = merken.id 
+		LEFT JOIN landen ON land_id = merken.id
+		ORDER BY id';
+
+// als search (submit) aangeklikt is, maak dan een variabele aan van de waarde in search en doe er iets veiligs mee zodat gebruiker niet eigen script kan invoeren
+// dan zoekwoord opslaan als variabele en toepassen in query als zoekfunctie
+
+
+
+/*
+ * Hier definiëer ik de standaard sorteer waarde
+ * Als de sorteer waarde is meegegeven dan vervang ik deze waarde
+ */
+$sorteer = 'ID';
+if( isset($_GET['sort']) ){  
+	$sorteer = mysqli_real_escape_string($con, htmlspecialchars($_GET['sort'])); 
+}
+
+/*
+ * In deze query zorg ik dat de ORDER by altijd volgens de sorteer waarde gebeurd
+ * Hierdoor heb ik geen andere query meer nodig
+ * Let op het quote gebruik bij de ORDER BY, ik gebruik daar geen quotes
+ */
+if( isset($_GET['search']) ){  
+  $zoekwoord = mysqli_real_escape_string($con, htmlspecialchars($_GET['search'])); 
+  $sql = "SELECT merken.id, merken.naam, merken.land, autos.title, autos.kleur, autos.type, autos.brandstof 
+  			FROM merken 
+  			LEFT JOIN autos ON merk_id = merken.id 
+  			LEFT JOIN landen ON land_id = merken.id
+  			WHERE naam LIKE '%{$zoekwoord}%' 
+  			OR type LIKE '%{$zoekwoord}%' 
+  			ORDER BY $sorteer";
+}
+
+//echo "Ik wil sorteren op:".$sorteer;
+$result = $con->query($sql);
+?>
+<div class="container"> 
+    <label>Zoek je auto</label>
+	<?php
+	/* 
+	 * Zoals je ziet heb ik hier de verschillende formulieren samen gevoegd zodat de waardes bewaard blijven
+	 */
+	?>
+    <form method="get">
+    	<!-- als zoekwoord is ingevuld, onthoudt de value dan -->
+        <input type="text" placeholder="Zoek je auto" name="search" value="<?php if (isset($_GET["search"])) {echo $zoekwoord;}?>">
+      	<select name="sort">
+      	<option value="naam" <?php if( 'naam' == $sorteer ) { echo 'selected'; } ?>>naam</option>
+  		<option value="land" <?php if( 'land' == $sorteer ) { echo 'selected'; } ?>>land</option>
+  		<option value="type" <?php if( 'type' == $sorteer ) { echo 'selected'; } ?>>type</option>
+  		<option value="kleur" <?php if( 'kleur' == $sorteer ) { echo 'selected'; } ?>>kleur</option>
+  		<option value="brandstof" <?php if( 'brandstof' == $sorteer ) { echo 'selected'; } ?>>brandstof</option>
+  		</select>		 
+ 		<input type="submit" value="Sort / Search" name="btn">
+    </form>
+    <h2>Lijst met auto's</h2>
+    <table>
+        <tr>
+			<th>Id</th>
+			<th>Naam</th>
+			<th>Land</th>
+			<th>titel</th>
+			<th>kleur</th>
+			<th>type</th>
+			<th>brandstof</th>
+        </tr>
+
+
+        <?php 
+        	//ga terug naar verse webpagina om volledig overzicht te zien
+        	echo "<a href='form.php'> Laat alle auto's zien</a>"; 
+        	// zolang er waarde is in de $row vul dan de tabel.
+	    	while($row = $result->fetch_assoc()){
+	        	echo "<tr>
+	                <td>".$row['id']."</td>
+	                <td>".$row['naam']."</td>
+	                <td>".$row['land']."</td>
+	              	<td>".$row['title']."</td>
+	                <td>".$row['kleur']."</td>
+	                <td>".$row['type']."</td>
+	                <td>".$row['brandstof']."</td> 
+	            	</tr>";
+
+	    	}
+	    	// als result geen waarde anders dan nul heeft, is er geen match gevonden tussen Database en zoekresultaat
+	    	if(0 ===mysqli_num_rows($result)){ 
+    			echo "Geen auto's gevonden met deze zoekopdracht";
+    		}
+        	
+        ?>
+    </table>
+</div>
+</main>
+<footer>
+	
+</footer>
+</body>	
+</html>
